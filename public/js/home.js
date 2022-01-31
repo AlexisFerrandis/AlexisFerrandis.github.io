580cc2f45750332a12914a3a35df7bd7727ab0e0
Alexis Ferrandis <alexis.1.ferrandis@gmail.com>
2022-01-31 18:32:10 +0100

    hr no video

--- public/js/home.js
+++ public/js/home.js
@@ -14,7 +14,7 @@ function carousel() {
 		myIndex = 1;
 	}
 	x[myIndex - 1].style.display = "block";
-	setTimeout(carousel, 9000);
+	setTimeout(carousel, 7000);
 }
 
 // LOGO OPACITY
@@ -53,7 +53,7 @@ window.addEventListener("scroll", (e) => {
 	e.preventDefault();
 
 	const headerDOM = document.getElementById("headerHomePage");
-	if (window.scrollY >= window.innerHeight - 60) {
+	if (window.scrollY >= window.innerHeight - 60 || window.innerHeight + window.pageYOffset >= document.offsetHeight) {
 		headerDOM.classList.remove("header-transparent");
 		headerDOM.classList.add("header-color-primary");
 		headerHamburger.style.filter = "invert(100%) sepia(1%) saturate(2%) hue-rotate(62deg) brightness(106%) contrast(101%)";
